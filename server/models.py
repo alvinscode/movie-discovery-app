5b33d9a8ccf5435457c798640236953ef05da3df
Alvin Li <aflip24@gmail.com>
2023-10-07 02:05:04 -0500

    create seed.py

--- server/models.py
+++ server/models.py
@@ -1,7 +1,30 @@
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
+
+user_favorite_genres = Table(
+    'user_favorite_genres',
+    db.Model.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('genre_id', Integer, ForeignKey('genre.id'))
+)
+
+user_movies = Table(
+    'user_movies',
+    db.Model.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('movie_id', Integer, ForeignKey('movie.id'))
+)
+
+movie_genre_association = Table(
+    'movie_genre_association',
+    db.Model.metadata,
+    Column('movie_id', Integer, ForeignKey('movie.id')),
+    Column('genre_id', Integer, ForeignKey('genre.id'))
+)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,11 +33,13 @@ class User(db.Model):
     password_hash = db.Column(db.String, nullable=False)
 
     reviews = db.relationship('Review', backref='user')
-    favorite_genres = db.relationship('Genre', backref='users')
+    favorite_genres = relationship("Genre", secondary=user_favorite_genres, backref="users")
+    rated_movies = relationship("Movie", secondary=user_movies, back_populates="rated_by_users")
+    watched_movies = relationship("Movie", secondary=user_movies, viewonly=True, back_populates="watched_by_users")
 
     def __repr__(self):
         return f'<User {self.username}>'
-    
+
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -22,13 +47,14 @@ class Movie(db.Model):
     description = db.Column(db.Text)
     average_rating = db.Column(db.Float, default=0)
 
-    users = db.relationship('User', backref='movies')
-    genres = db.relationship('Genre', backref='movies')
+    genres = relationship("Genre", secondary=movie_genre_association, backref="movies")
     reviews = db.relationship('Review', backref='movie')
 
+    rated_by_users = relationship("User", secondary=user_movies, back_populates="rated_movies")
+    watched_by_users = relationship("User", secondary=user_movies, viewonly=True, back_populates="watched_movies")
     def __repr__(self):
         return f'<Movie {self.title}>'
-    
+
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
@@ -39,10 +65,10 @@ class Review(db.Model):
 
     def __repr__(self):
         return f'<Review by {self.user_id} for {self.movie_id}>'
-    
+
 class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f'<Genre {self.name}>'
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,59 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Movie, Genre, User, Review
+
+def create_or_get_genre(genre_name):
+    existing_genre = Genre.query.filter_by(name=genre_name).first()
+
+    if existing_genre:
+        return existing_genre
+    else:
+        new_genre = Genre(name=genre_name)
+        db.session.add(new_genre)
+        db.session.commit()
+        return new_genre
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+
+        action_genre = create_or_get_genre('Action')
+        drama_genre = create_or_get_genre('Drama')
+        comedy_genre = create_or_get_genre('Comedy')
+
+        movies = []
+        for _ in range(10):
+            title = fake.catch_phrase()
+            release_year = fake.year()
+            description = fake.paragraph()
+            movie = Movie(title=title, release_year=release_year, description=description)
+            movie.genres.append(rc([action_genre, comedy_genre, drama_genre]))
+            movies.append(movie)
+
+        db.session.add_all(movies)
+        db.session.commit()
+
+        users = []
+        reviews = []
+
+        for _ in range(5):
+            username = fake.user_name()
+            email = fake.email()
+            password_hash = 'hashed_password'
+
+            user = User(username=username, email=email, password_hash=password_hash)
+            users.append(user)
+
+            review_text = fake.paragraph()
+            rating = fake.pydecimal(min_value=1, max_value=5, right_digits=1)
+            movie = rc(movies)
+
+            review = Review(text=review_text, rating=rating, user=user, movie=movie)
+            reviews.append(review)
+
+        db.session.add_all(users + reviews)
+        db.session.commit()
+
+        print("Seed complete.")
